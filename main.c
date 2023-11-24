f07bd0bf6a7349626de053bc4a1813ad2dc8d8fe
Rayan-Daoudi <133090737+Rayan-Daoudi@users.noreply.github.com>
2023-11-24 09:41:50 +0100

    Update main.c
    recuperer des info des dossier

--- main.c
+++ main.c
@@ -3,3 +3,34 @@
 //
 
 #include "main.h"
+
+
+t_graphe lecture (charfile){
+    t_graphe graphe = malloc (sizeof (t_graphe ));
+    FILEfichier = fopen(file,"r");
+    if (!fichier)
+    {
+        printf("Erreur de lecture fichier\n");
+        exit(-1);
+    }
+    int orientation;
+    int ordre;
+
+
+    int sommet[50][2];
+    int h = 0;
+    char actuel;
+    while (actuel != EOF){
+        actuel = fgetc(fichier);
+        if (actuel >= '0' && actuel<= '9') {
+            fseek(fichier, -1,SEEK_CUR);
+            fscanf(fichier, "%d %d", &sommet[h][0],&sommet[h][1]);
+            h++;
+        }
+    }
+    ordre = h;
+    for (int i =0;i<ordre;i++){
+        printf("\n%d %d",sommet[i][0],sommet[i][1]);
+    }
+return graphe
+}
