f44f5682aa2ea82e799863e1c2ab11791fd51aa9
Alexiakz <131662539+Alexiakz@users.noreply.github.com>
2023-12-03 19:57:04 +0100

    Create exclusion.h

---
+++ exclusion.h
@@ -0,0 +1,7 @@
+#ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD1_EQUIPE4_EXCLUSION_H
+#define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD1_EQUIPE4_EXCLUION_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
